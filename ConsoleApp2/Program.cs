9c555c7135320ef9aff184ab9f3fdff3626674f4
Пользователь <@DESKTOP-CAJR7TQ>
2022-11-13 20:33:25 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,7 @@
+﻿
+{ int a3 = 3; int a2 = 5;
+    int a1 = 0; int b2 = 5; int b1 = 0; 
+    int a = a3 * 100 + a2 * 10 + a1; int b = b2 * 10 + b1; 
+    int c = a + b; string number = Convert.ToString(c); 
+    Console.WriteLine($"c3 {number[0]}, c2 {number[1]}, c1 {number[2]}"); 
+}
